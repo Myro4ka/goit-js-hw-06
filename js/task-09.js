d35292338e289aa805c6d3d6a62841a34b3e0055
Myroslava Siryk <www.mirosya@gmail.com>
2022-10-07 02:40:05 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", onColorButton);
+
+function onColorButton() {
+  colorValue.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorValue.textContent;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
