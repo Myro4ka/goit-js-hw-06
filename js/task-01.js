35583e2a68aef4d13b7298898d46842fd4759d39
Myroslava Siryk <www.mirosya@gmail.com>
2022-10-07 16:58:52 +0300

    Automate output to the console in task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,30 +1,11 @@
 const listCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${listCategories.length}`);
 
-function getCategoryDetails(name, quantity) {
-  console.log(`Category: ${name}`);
-  console.log(`Elements: ${quantity}`);
-}
-const firstCategory = document.querySelector("#categories").firstElementChild;
-const nextCategory = firstCategory.nextElementSibling;
-const lastCategory = document.querySelector("#categories").lastElementChild;
-
-function getNameCategory(item) {
-  return item.querySelector("h2").textContent;
-}
-function getValueCategory(item) {
-  return item.querySelector("ul").children.length;
+function getCategoryDetails(list) {
+  for (let item of list) {
+    console.log(`Category: ${item.querySelector("h2").textContent}`);
+    console.log(`Elements: ${item.querySelector("ul").children.length}`);
+  }
 }
 
-getCategoryDetails(
-  getNameCategory(firstCategory),
-  getValueCategory(firstCategory)
-);
-getCategoryDetails(
-  getNameCategory(nextCategory),
-  getValueCategory(nextCategory)
-);
-getCategoryDetails(
-  getNameCategory(lastCategory),
-  getValueCategory(lastCategory)
-);
+getCategoryDetails(listCategories);
